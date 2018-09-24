f065c540059e1db6314293e2cc58a50d4bf886a1
Leonardo Garcia <leonardo.garcia@wizeline.com>
2018-09-24 09:41:45 -0500

    Change programmatic map view

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+
+    var mapView: MKMapView!
+
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+
+        // Set it as the view of this view controller
+        view = mapView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
